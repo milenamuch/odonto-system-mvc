33c13c24a2debf9fb78bf293c87b8f24e7e4193f
milenamuch <83710278+milenamuch@users.noreply.github.com>
2022-03-03 22:06:59 -0300

    Add files via upload

---
+++ Migrations/20220304004123_InitialDB.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ConsultorioOdontologico.Migrations
+{
+    public partial class InitialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Agendamentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    PacienteId = table.Column<int>(nullable: false),
+                    DentistaId = table.Column<int>(nullable: false),
+                    SalaId = table.Column<int>(nullable: false),
+                    Data = table.Column<DateTime>(nullable: false),
+                    Confirmado = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agendamentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Dentistas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Fone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    Registro = table.Column<string>(nullable: false),
+                    Salario = table.Column<double>(nullable: false),
+                    EspecialidadeId = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Dentistas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Especialidades",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: false),
+                    Detalhamento = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Especialidades", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Pacientes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(nullable: true),
+                    Cpf = table.Column<string>(nullable: true),
+                    Fone = table.Column<string>(nullable: true),
+                    Email = table.Column<string>(nullable: true),
+                    Senha = table.Column<string>(nullable: true),
+                    DataNascimento = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacientes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Procedimentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Descricao = table.Column<string>(nullable: false),
+                    Preco = table.Column<double>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Procedimentos", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Salas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Numero = table.Column<string>(nullable: false),
+                    Equipamentos = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Salas", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AgendamentoProcedimentos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    ProcedimentoId = table.Column<int>(nullable: false),
+                    AgendamnentoId = table.Column<int>(nullable: false),
+                    AgendamentoId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgendamentoProcedimentos", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_AgendamentoProcedimentos_Agendamentos_AgendamentoId",
+                        column: x => x.AgendamentoId,
+                        principalTable: "Agendamentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_AgendamentoProcedimentos_Procedimentos_ProcedimentoId",
+                        column: x => x.ProcedimentoId,
+                        principalTable: "Procedimentos",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendamentoProcedimentos_AgendamentoId",
+                table: "AgendamentoProcedimentos",
+                column: "AgendamentoId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendamentoProcedimentos_ProcedimentoId",
+                table: "AgendamentoProcedimentos",
+                column: "ProcedimentoId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgendamentoProcedimentos");
+
+            migrationBuilder.DropTable(
+                name: "Dentistas");
+
+            migrationBuilder.DropTable(
+                name: "Especialidades");
+
+            migrationBuilder.DropTable(
+                name: "Pacientes");
+
+            migrationBuilder.DropTable(
+                name: "Salas");
+
+            migrationBuilder.DropTable(
+                name: "Agendamentos");
+
+            migrationBuilder.DropTable(
+                name: "Procedimentos");
+        }
+    }
+}
